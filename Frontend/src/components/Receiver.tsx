7981bfbab2671a47edf94a83e1627238aa018057
Ashutosh <kumarashutosh1956@gmail.com>
2025-01-06 14:55:41 +0530

    creating offer, answer and ice-candidate

--- Frontend/src/components/Receiver.tsx
+++ Frontend/src/components/Receiver.tsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import  { useEffect } from 'react'
 
 export default function Receiver() {
+  // const [receiverSocket, setReceiverSocket] = useState<WebSocket|null>(null);
+  useEffect(()=>{
+    const socket = new WebSocket('ws://localhost:8080');
+    socket.onopen=()=>{
+      socket.send(JSON.stringify({type:"receiver"}));
+    }
+
+    socket.onmessage= async (event)=>{
+      const message = JSON.parse(event.data);
+      let pc =  new RTCPeerConnection();
+      if(message.type==="createOffer"){
+        pc.setRemoteDescription(message.sdp);
+        pc.onicecandidate = (event)=>{
+          if(event.candidate){
+            socket?.send(JSON.stringify({type:'iceCandidate', candidate:event.candidate}));
+          }
+        }
+
+        pc.ontrack=(track)=>{
+          console.log(track);
+        }
+        const answer = await pc.createAnswer();
+        pc.setLocalDescription(answer);
+        socket.send(JSON.stringify({type:"createAnswer", sdp:pc.localDescription}));
+
+      }
+      else if(message.type==='iceCandidate'){
+        pc.addIceCandidate(message.iceCandidate);
+      }
+    }
+  },[])
   return (
     <div>
       Receiver
--- Frontend/src/components/Sender.tsx
+++ Frontend/src/components/Sender.tsx
@@ -1,15 +1,47 @@
-import React, { useEffect } from 'react'
+import {useEffect, useState } from 'react'
 
 export default function Sender() {
+  const [senderSocket, setSenderSocket] = useState<WebSocket|null>(null);
     useEffect(()=>{
         const socket = new WebSocket('ws://localhost:8080');
         socket.onopen=()=>{
             socket.send(JSON.stringify({type:"sender"}))
         }
+        setSenderSocket(socket);
     },[])
+
+    async function startSendingVideo(){
+      if(!senderSocket) return;
+      const pc = new RTCPeerConnection();
+      pc.onnegotiationneeded = async () =>{
+        console.log("negotiated");
+        const offer = await pc.createOffer();
+        await pc.setLocalDescription(offer);
+        senderSocket?.send(JSON.stringify({type:'createOffer', sdp:pc.localDescription}));
+      }
+     
+      pc.onicecandidate = (event)=>{
+        if(event.candidate){
+          senderSocket?.send(JSON.stringify({type:'iceCandidate', candidate:event.candidate}));
+        }
+      }
+
+      senderSocket.onmessage=(event)=>{
+        const message = JSON.parse(event.data);
+
+        if(message.type==='createAnswer'){
+          pc.setRemoteDescription(message.data);
+        }
+        else if(message.type==="iceCandidate"){
+          pc.addIceCandidate(message.candidate);
+        }
+      }
+      const vdoStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false});
+      pc.addTrack(vdoStream.getVideoTracks()[0]);
+    }
   return (
     <div>
-      Sender
+      <button onClick={startSendingVideo}>Sender</button>
     </div>
   )
 }
